38be804530b058d0cf2b49e1e7491992604aa67c
KSA <kiran.sasr@gmail.com>
2020-12-10 19:06:31 -0500

    Initial Commit to CsharpSample

---
+++ Csharpnetcoresample/UnitTest1.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Csharpnetcoresample
+{
+    public class Tests
+    {
+        public IWebDriver Driver;
+
+        [SetUp]
+        
+        public void Setup()
+        {
+
+            Console.WriteLine("This is a setup test");
+
+            Driver = new ChromeDriver();
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com");
+
+            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+
+            Driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+            
+            
+            Console.WriteLine("This is a sample test");
+
+            Assert.Pass();
+        }
+    }
+}
